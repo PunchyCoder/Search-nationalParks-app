53d87e151c32cff91799c3269204cce3c4e4dc0b
PunchyCoder <37468332+PunchyCoder@users.noreply.github.com>
2019-05-04 13:04:18 -0700

    Update main.js
    cleaned up MOST of the debugging console.logs.
    deleted comments

--- main.js
+++ main.js
@@ -4,8 +4,6 @@
 // -- -- --GLOBAL VARIABLES-- -- --
 const searchURL = 'https://developer.nps.gov/api/v1/parks';
 const apiKey = '43MwYCVPjSmg3YJxXEaUwf3BM9vH1RvRviKofC1P';
-let statesList = []; // when state field is entered, push state code to array --statesList
-
 
 function formatQueryParams(params) { //   not combining params, nedds debugging...
   const queryItems = Object.keys(params)
@@ -15,15 +13,11 @@ function formatQueryParams(params) { //   not combining params, nedds debugging.
 
 
 function displayResults(responseJson) {
-  // empty previous results
   $(".results").empty();
   $(".results").append(`<h2 style="color:orange;">Search Results</h2>`);
 
-  // loop through results
   for (let i = 0; i < responseJson["data"].length-1; i++) {
 
-    console.log("displaying results...");
-    //   --append newResult
     $(".results").append(`<li>
           <h3>${responseJson["data"][i].fullName}, ${responseJson["data"][i].states} </h3>
           <p>${responseJson["data"][i].description}</p>
@@ -50,30 +44,17 @@ function getResponse(query, max, state) {
   const url = searchURL + '?' + queryString;
   console.log(url);
 
-fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson))
-    .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    });
-
-  // fetch(url)
-  // .then(response => {
-  //   if (response.ok) {
-  //     return response.json();
-  //     } else {
-  //       throw new Error(response.statusText)
-  //     }
-  //   })
-  // .then((responseJson) => {
-  //   displayResults(responseJson)})
-  // .catch(err => { console.log(err.message)//append as HTML to <error message>
-  // })
+  fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error(response.statusText);
+      })
+      .then(responseJson => displayResults(responseJson))
+      .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      });
 }
 
 function watchForm() {
@@ -82,13 +63,10 @@ function watchForm() {
     const stateCode = $('.js-state-search').val();
     const searchTerms = $('.js-searchTerm').val();
     let maxResults = $('.js-maxResults').val();
-    console.log(maxResults)
     if (maxResults == '') {
       maxResults = 10;
     }
-    console.log(maxResults)
     getResponse(searchTerms, maxResults, stateCode);
-
   });
 }
 
