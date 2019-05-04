a6858bd6ab1d49197c04e69fb5e196b22ba10da8
PunchyCoder <37468332+PunchyCoder@users.noreply.github.com>
2019-05-04 11:04:06 -0700

    Update files via upload
    uploaded updated index, styles, and main files

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-
+ 
 "use strict";
+
+// const stateAbbrvList = [  'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL',
+//                           "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", 'MO', 'MT', 
+//                           "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI",
+//                           "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY" ]
 
 const searchURL = 'https://developer.nps.gov/api/v1/parks';
 const apiKey = '43MwYCVPjSmg3YJxXEaUwf3BM9vH1RvRviKofC1P';
@@ -7,79 +12,86 @@ let statesList = []; // when state field is entered, push state code to array --
 
 function formatQueryParams(params) { //   not combining params, nedds debugging...
   const queryItems = Object.keys(params)
-    .map(key => {
-      `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`})
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
     return queryItems.join('&');
 }
 
-function displayResults(response) {
-  console.log(response);
-  // empty results
+
+function displayResults(responseJson) {
+  // empty previous results
   $(".results").empty();
-  
+
   // loop through results
-  for (let i = 0; i < response.length; i++) {
+  for (let i = 0; i < responseJson["data"].length; i++) {
+
+    console.log("displaying results...");
     //   --append newResult
-    $(".results").append(
-      `<li><h3>${responseJson[i].fullName}</h3>
+    $(".results").append(`<li>
+          <h3>${responseJson["data"][i].fullName}</h3>
           <p></p>
       </li>`
     );
   }
 }
 
-function getResponse(query, maxResults=10, states) {
+function getResponse(query, max, state) {
   const params = 
   { 
-    key: apiKey,
+    api_key: apiKey,
     q: query,
-    stateCode: states,
-    maxResults
+    stateCode: state,
+    limit: max
   };
 
-  console.log(params["q"]);
-  console.log(params["maxResults"]);
-  console.log(params["stateCode"]);
-
-  console.log()
+  console.log(`q = ${params["q"]}`);
+  console.log(`limit = ${params["limit"]}`);
+  console.log(`stateCodes = ${params["stateCode"]}`);
 
   const queryString = formatQueryParams(params);
   const url = searchURL + '?' + queryString;
-
   console.log(url);
 
-  fetch(url)
-  .then(response => {
-    if (response.ok) {
-      return response.json();
+fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
       }
-    throw new Error(response.statusText);
+      throw new Error(response.statusText);
     })
-  .then(responseJson => displayResults(responseJson))
-  .catch(err => { console.log(err.message);//append as HTML to <error message>
-  });
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+
+  // fetch(url)
+  // .then(response => {
+  //   if (response.ok) {
+  //     return response.json();
+  //     } else {
+  //       throw new Error(response.statusText)
+  //     }
+  //   })
+  // .then((responseJson) => {
+  //   displayResults(responseJson)})
+  // .catch(err => { console.log(err.message)//append as HTML to <error message>
+  // })
 }
 
-// function addStateCode() {
-//   console.log('Executing...')
-//   $('js-add-state').on("click", function() {
-//     console.log('adding state...')
-//     const newStateCode = $('js-state-search').val();
-//     let statesList = [];
-//     statesList.push(newStateCode);
-//     $('js-states-list').append(`<li>${newStateCode}</li>`);
-//     $('js-state-search').val('')
-//   });
-// };
-
 function watchForm() {
-  
   $('form').submit(event => {
     event.preventDefault();
+    const stateCode = $('.js-state-search').val();
     const searchTerms = $('.js-searchTerm').val();
-    const maxResults = $('.js-maxResults').val();
-    getResponse(searchTerms, maxResults, statesList)
+    let maxResults = $('.js-maxResults').val();
+    
+    if (maxResults === undefined) {
+      maxResults = 10;
+    }
+    getResponse(searchTerms, maxResults, stateCode);
+
   });
 }
 
 $(watchForm);
+
+ 
