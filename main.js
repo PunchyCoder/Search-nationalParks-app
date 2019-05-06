a29afac4cc394f0ce5c5e63889f6a38413f9ab95
PunchyCoder <37468332+PunchyCoder@users.noreply.github.com>
2019-05-06 14:10:59 -0700

    Updated main.js
    Added states list to check against when adding states to search...

--- main.js
+++ main.js
@@ -2,10 +2,13 @@
 "use strict";
 
 // -- -- --GLOBAL VARIABLES-- -- --
+const state_Codes = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'];
+
 const searchURL = 'https://developer.nps.gov/api/v1/parks';
 const apiKey = '43MwYCVPjSmg3YJxXEaUwf3BM9vH1RvRviKofC1P';
+let stateList = [];
 
-function formatQueryParams(params) { //   not combining params, nedds debugging...
+function formatQueryParams(params) { 
   const queryItems = Object.keys(params)
     .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
     return queryItems.join('&');
@@ -27,34 +30,34 @@ function displayResults(responseJson) {
   }
 }
 
-function getResponse(query, max, state) {
+function getResponse(query, max, stateList) {
   const params = 
   { 
     api_key: apiKey,
     q: query,
-    stateCode: state,
+    stateCode: stateList,
     limit: max
   };
 
-  console.log(`q = ${params["q"]}`);
-  console.log(`limit = ${params["limit"]}`);
-  console.log(`stateCodes = ${params["stateCode"]}`);
+  console.log(`q = ${params["q"]}`); // console.log
+  console.log(`limit = ${params["limit"]}`); // console.log
+  console.log(`stateCodes = ${params["stateCode"]}`); // console.log
 
   const queryString = formatQueryParams(params);
   const url = searchURL + '?' + queryString;
-  console.log(url);
+  console.log(url); // console.log
 
-  fetch(url)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error(response.statusText);
-      })
-      .then(responseJson => displayResults(responseJson))
-      .catch(err => {
-        $('#js-error-message').text(`Something went wrong: ${err.message}`);
-      });
+fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
 }
 
 function addStates(state) {
